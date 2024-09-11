c669b539b1a2bea06554a73fc2b9380b79fcdd36
Difroy <unifroy@gmail.com>
2024-09-11 03:39:03 +0200

    Update Industry.java

--- com/generation/tropico/model/entities/Industry.java
+++ com/generation/tropico/model/entities/Industry.java
@@ -1,5 +1,14 @@
 package com.generation.tropico.model.entities;
 
-public class Industry {
+public class Industry extends Activity {
+
+	
+	protected ProductType productType;
+	protected int production;
+	
+	public Industry(int id, int x2, int x1, int y2, int y1, String name, int salary, int jobs) {
+		super(id, x2, x1, y2, y1, name, salary, jobs);
+		
+	}
 
 }
